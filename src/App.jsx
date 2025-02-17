9cfc77a0bb0c36bba70651200ecfa15c6d2cef8a
Mohd Aquib <akibkhan9984@gmail.com>
2025-02-18 03:34:12 +0530

    navbar component done with sidebar control on menu button click

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,16 @@ import './App.css'
 import { BrowserRouter } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
 import { useDashboardContext } from "./context/DashboardContext";
+import Navbar from './components/Navbar';
 
 export default function App() {
   const {activeMenu} = useDashboardContext()
   return (
     <>
     <BrowserRouter>
-      <div className="bg-white">
+        <div className='flex relative bg-white'>
           {activeMenu ? (
-            <div className="w-64 fixed bg-white shadow-xl border-r border-gray-200">
+            <div className="w-64 fixed bg-white shadow-xl border-r border-gray-200 z-20">
               <Sidebar />
             </div>
           ) : (
@@ -18,6 +19,12 @@ export default function App() {
               <Sidebar />
             </div>
           )}
+          <div className={`min-h-screen w-full ${activeMenu ? 'md:ml-64' : 'flex-2'}`}>
+            <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
+              <Navbar />
+            </div>
+
+          </div>
       </div>
     </BrowserRouter>
     </>
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,48 @@
+import { RiInboxArchiveFill } from "react-icons/ri";
+import { AiOutlineMenu } from "react-icons/ai";
+import { useDashboardContext } from "../context/DashboardContext";
+import { useEffect } from "react";
+const Navbar = () => {
+    const { screenSize, setScreenSize, setActiveMenu } = useDashboardContext();
+    useEffect(() => {
+        const handleResize = () => setScreenSize(window.innerWidth);
+        window.addEventListener("resize", handleResize);
+        handleResize();
+
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
+
+    useEffect(() => {
+        if (screenSize <= 900) {
+            setActiveMenu(false);
+        } else {
+            setActiveMenu(true);
+        }
+    }, [screenSize, setActiveMenu]);
+    return (
+        <nav className="flex justify-between md:px-2 px-5 py-1 items-center shadow-lg">
+            <div className="flex gap-5">
+                <button title="menu"
+                    onClick={() => setActiveMenu((prevMenu) => !prevMenu)}
+                >
+                    <AiOutlineMenu fill="#6666f2" />
+                </button>
+                <div className="md:block hidden">
+                    <h2 className="text-lg text-slate-800 font-bold capitalize">expense management</h2>
+                    <p className="text-slate-500 capitalize text-sm font-semibold">manage employee expense reimbursements</p>
+                </div>
+            </div>
+            <div className="flex gap-4 items-center">
+                <button className="flex gap-1 items-center text-sm font-semibold">
+                    <RiInboxArchiveFill className="text-lg" />
+                    My Inbox
+                </button>
+                <avatar className="h-10 w-10 bg-[#eb996e] rounded-full flex justify-center items-center text-white">
+                    A
+                </avatar>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
